870167d63709623e6ec62360b378f72083080c59
anmolkatia <47606832+anmolkatia@users.noreply.github.com>
2020-04-25 14:15:54 +0530

    Update settings.py

--- anmol/settings.py
+++ anmol/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1=l%#gz)e@lwsx-kj%knltudw^vkt_q9-7&$$xzr5ro&y7ftd)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
